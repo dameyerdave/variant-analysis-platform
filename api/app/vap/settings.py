40e420da1b903e3ec1b3e9822c9c180014a25c7a
dameyerdave <dameyerdave@gmail.com>
2022-07-20 14:01:29 +0200

    increased token lifetimes

--- api/app/vap/settings.py
+++ api/app/vap/settings.py
@@ -147,8 +147,8 @@ FIXTURE_DIRS = []
 
 ### JWT ###
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': td(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': td(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': td(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': td(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
@@ -175,9 +175,9 @@ SIMPLE_JWT = {
 
     'JTI_CLAIM': 'jti',
 
-    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': td(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': td(days=1),
+    # 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    # 'SLIDING_TOKEN_LIFETIME': td(minutes=30),
+    # 'SLIDING_TOKEN_REFRESH_LIFETIME': td(days=1),
 }
 
 # JWT_AUTH = {
